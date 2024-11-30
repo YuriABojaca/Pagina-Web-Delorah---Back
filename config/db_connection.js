783b8180fe8d7b65c44bd490d999f19866e9fbc2
YuriABojaca <yuri.al26@hotmail.com>
2024-11-30 11:00:20 -0500

    commit
    Commit

--- config/db_connection.js
+++ config/db_connection.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 // Configurar conexión a PostgreSQL
 const pool = new Pool({
-    user: 'delorah_user',       // Reemplaza con tu usuario de PostgreSQL
-    host: 'localhost',
-    database: 'delorah',     // Nombre de la base de datos
-    password: 'delorah.user123', // Reemplaza con tu contraseña de PostgreSQL
+    user: 'postgres',       
+    host: 'postgresql://postgres:vhQEgpGEiHKWgSYuYFlPhTDaJRDyumPF@autorack.proxy.rlwy.net:40937/railway',
+    database: 'railway',     
+    password: 'vhQEgpGEiHKWgSYuYFlPhTDaJRDyumPF',
     port: 5432,
 });
 
